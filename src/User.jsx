ec9afa279dbb6f880c64094100dd453ec59ca88a
Denis Chornyi <chornyi.den@gmail.com>
2024-09-17 18:16:48 +0300

    fix

--- src/User.jsx
+++ src/User.jsx
@@ -1,70 +1,30 @@
-// import { useParams } from 'react-router-dom';
-// import { useEffect, useState } from 'react';
-
-// const User = () => {
-//   const [userData, setUserData] = useState({
-//     avatar_url: null,
-//     name: null,
-//     location: null
-//   });
-
-//   const { userId } = useParams();
-
-//   useEffect(() => {
-//     fetch(`https://api.github.com/users/${userId}`)
-//       .then(response => response.json())
-//       .then(({ avatar_url, name, location }) =>
-//         setUserData({
-//           avatarUrl: avatar_url,
-//           name,
-//           location
-//         })
-//       );
-//   }, [userId]);
-//   const { avatar_url, name, location } = userData;
-//   return (
-//     <div className="user">
-//       <img alt="User Avatar" src={avatar_url} className="user__avatar" />
-//       <div className="user__info">
-//         <span className="user__name">{name}</span>
-//         <span className="user__location">{location}</span>
-//       </div>
-//     </div>
-//   );
-// };
-
-// // TODO update solutiuins here
-
-// export default User;
-import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const User = () => {
-  const { userId } = useParams();
-  const [user, setUser] = useState({
-    avatarUrl: null,
+  const [userData, setUserData] = useState({
+    avatar_url: null,
     name: null,
-    location: null,
+    location: null
   });
+
+  const { userId } = useParams();
 
   useEffect(() => {
     fetch(`https://api.github.com/users/${userId}`)
       .then(response => response.json())
       .then(({ avatar_url, name, location }) =>
-        setUser({
+        setUserData({
           avatarUrl: avatar_url,
           name,
-          location,
-        }),
+          location
+        })
       );
   }, [userId]);
-
-  const { avatarUrl, name, location } = user;
-  if (avatarUrl === null || name === null || location === null) return null;
-
+  const { avatar_url, name, location } = userData;
   return (
     <div className="user">
-      <img src={avatarUrl} className="user__avatar" alt="User Avatar" />
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
       <div className="user__info">
         <span className="user__name">{name}</span>
         <span className="user__location">{location}</span>
